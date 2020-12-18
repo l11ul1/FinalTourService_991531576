8a44f21ff0a0c3f0ab803f9342c464e7c9125bbe
Vladislav Mun <munv@sheridancollege.ca>
2020-12-18 15:39:55 -0500

    init commit

---
+++ FinalTourService_991531576/TourService_991531576.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FinalTourService_991531576
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TourService_991531576" in both code and config file together.
+    public class TourService_991531576 : ITourService_991531576
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
