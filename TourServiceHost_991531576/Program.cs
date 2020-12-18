98b72694d26ae8c96bc451eb93eda44952c07521
Vladislav Mun <munv@sheridancollege.ca>
2020-12-18 16:00:29 -0500

    added/configuered host to solution

---
+++ TourServiceHost_991531576/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourServiceHost_991531576
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(FinalTourService_991531576.TourService_991531576)))
+            {
+                host.Open();
+                Console.WriteLine("Prog30000 final exam by Mun Vladislav 991531576");
+                Console.WriteLine("Host started at:" + DateTime.Now);
+                Console.ReadKey();
+            }
+        }
+    }
+}
